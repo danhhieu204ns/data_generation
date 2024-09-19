44ea209e85bb14a5cbc37c35fd3923427ae0f29c
Danh Hieu <danhhieu123ns@gmail.com>
2024-09-19 09:24:23 +0700

    init

--- utils/func.py
+++ utils/func.py
@@ -1,7 +1,7 @@
 import fitz, nltk
 import numpy as np
 from pyvi.ViTokenizer import tokenize
-import re
+import re, csv, os
 # nltk.download('punkt_tab')
 
 def clean_string(s):
@@ -42,11 +42,34 @@ def split_into_sentences(documents):
 
     sentence_lists = []
     sentences = nltk.sent_tokenize(documents)
+    
     for sent in sentences:
         # cleaned = clean_string(sent)
         # if len(cleaned.split(" ")) > 4: 
         #     sentence_lists.append(cleaned)
         if len(sent.split(" ")) > 4: 
             sentence_lists.append(sent)
+
     sentence_lists = np.array(sentence_lists)
     return sentence_lists
+
+
+def count_file(db_path, analize_path): # thong ke so luong file cua folder
+    db_names = ["Accountant", "IT", "Japanese", "Luật", "QTKD"]
+    data = []
+
+    for db_name in db_names:
+        folder_path = os.path.join(db_path, db_name)
+        pdf_files = [f for f in os.listdir(folder_path)]
+        data.append({
+            "Ngành học": db_name, 
+            "Số lượng file": len(pdf_files)
+        })
+
+    outfile_path = os.path.join(analize_path, f"quantity_pdf.csv")
+
+    fieldnames = data[0].keys()
+    with open(outfile_path, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
